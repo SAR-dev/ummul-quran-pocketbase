7d0fd2796c4f5afb1efdf84cdbcf0b7482ae9f90
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-12-27 00:12:19 +0900

    up

--- frontend/src/pages/TeacherSelf.tsx
+++ frontend/src/pages/TeacherSelf.tsx
@@ -8,15 +8,15 @@ import ClassStats from "../components/ClassStats";
 const TeacherSelf = () => {
   return (
     <NavLayout>
-      <div className="grid grid-cols-1 md:grid-cols-7 p-5 md:p-16 gap-10 w-full">
-        <div className="col-span-1 md:col-span-5">
+      <div className="grid grid-cols-1 lg:grid-cols-7 p-5 lg:p-16 gap-10 w-full">
+        <div className="col-span-1 lg:col-span-5">
           <div className="w-full grid grid-cols-1 gap-10">
             <TodayClassList />
             <ClassCalendar />
             <StudentList />
           </div>
         </div>
-        <div className="col-span-1 md:col-span-2">
+        <div className="col-span-1 lg:col-span-2">
           <div className="grid grid-cols-1 gap-10">
             <TeacherInfo />
             <ClassStats />
