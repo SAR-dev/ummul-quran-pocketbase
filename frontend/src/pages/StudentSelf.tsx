a34dd33db504ee621a57946b7bea0b9ae8ef3cec
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-12-27 00:16:18 +0900

    up

--- frontend/src/pages/StudentSelf.tsx
+++ frontend/src/pages/StudentSelf.tsx
@@ -5,13 +5,13 @@ import StudentInvoiceList from '../components/StudentInvoiceList'
 const StudentSelf = () => {
     return (
         <NavLayout>
-            <div className="grid grid-cols-1 lg:grid-cols-7 p-5 lg:p-16 gap-10 w-full">
-                <div className="col-span-1 lg:col-span-5">
+            <div className="grid grid-cols-1 xl:grid-cols-7 p-5 xl:p-16 gap-10 w-full">
+                <div className="col-span-1 xl:col-span-5">
                     <div className="grid grid-cols-1 gap-10">
                         <StudentInvoiceList />
                     </div>
                 </div>
-                <div className="col-span-1 lg:col-span-2">
+                <div className="col-span-1 xl:col-span-2">
                     <div className="grid grid-cols-1 gap-10">
                         <StudentInfo />
                     </div>
